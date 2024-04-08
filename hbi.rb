6c081f1ed487da17d34316353446a90eb20f7fb2
pd3v <paulo.develop@gmail.com>
2024-04-08 16:09:33 +0100

    add formula

---
+++ hbi.rb
@@ -0,0 +1,18 @@
+class Hbi < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/pd3v/hbi/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "77a2425e7cc1f6a0267780b1af053f41c808b62101112db6ca544a099f0230f5"
+  license ""
+
+  depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if they cause configure to fail
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+end
