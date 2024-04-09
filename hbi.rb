f8121b568c8aa9283aeb56dd37e21a33aa59e7e6
pd3v <paulo.develop@gmail.com>
2024-04-09 17:27:39 +0100

    cmake set path to source code

--- hbi.rb
+++ hbi.rb
@@ -11,8 +11,8 @@ class Hbi < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    #system "./configure", "--disable-silent-rules", *std_configure_args
+    system "cmake", "-S", "src/.", "-B", "build", *std_cmake_args
   end
 
 end
