7cb07d76c62a13d13c0a360ca6023386a1cb5ca7
pd3v <paulo.develop@gmail.com>
2024-04-13 15:45:49 +0100

    testing installing

--- hbi.rb
+++ hbi.rb
@@ -12,8 +12,9 @@ class Hbi < Formula
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     #system "./configure", "--disable-silent-rules", *std_configure_args
-    system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
-    system "make", "-C", "build/"
+    #system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
+    #system "make", "-C", "build/"
+    bin.install "hbi"
   end
 
 end
