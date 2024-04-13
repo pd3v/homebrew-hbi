607574d169bd1773ca92bf6726b60690eae5322c
pd3v <paulo.develop@gmail.com>
2024-04-13 16:48:32 +0100

    testing installing

--- hbi.rb
+++ hbi.rb
@@ -12,8 +12,9 @@ class Hbi < Formula
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if they cause configure to fail
     # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "mk", "build", "cd", "build", "cmake.."
     system "./configure", "--disable-silent-rules", *std_configure_args
-    system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
+    #system "cmake", "-S", ".", "-B", "build/" , *std_cmake_args
     #system "make", "-C", "build/"
     bin.install "hbi"
   end
